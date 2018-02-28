605bbecdc8fdea17a1598be276659474917c398d
vicky <35688357+vigneshlove@users.noreply.github.com>
2018-02-28 15:54:40 +0530

    Create String(28-02-2018).C

---
+++ String(28-02-2018).C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void) 
+{
+	char str[10];
+	int n,i;
+	printf("Enter the string:");
+	scanf("%s",str);
+	n=strlen(str);
+	for(i=0;i<n;i++)
+	{
+		if(str[i]>='0' && str[i]<='9')
+		{
+			printf("\n YES");
+		}
+		else
+		printf("\n NO");
+	}
+	return 0;
+}
