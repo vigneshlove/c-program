53f19375572592f92c30e0bafd2eefc3fda08b06
vicky <35688357+vigneshlove@users.noreply.github.com>
2018-03-02 13:36:07 +0530

    Create Count the line given paragraph(02-03-2018).C

---
+++ Count the line given paragraph(02-03-2018).C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) 
+{
+
+	char str[50];
+	int i,count=1;
+	printf("Enter the sentence:");
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]=='.')
+		{
+			count=count+1;
+		}
+	}
+	printf("\nNo of lines is=%d",count);
+	return 0;
+}
