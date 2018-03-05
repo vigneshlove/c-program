85b525733f35440defb7b93a6f2be7de30fbd167
vicky <35688357+vigneshlove@users.noreply.github.com>
2018-03-05 16:12:10 +0530

    Update swap 2 number(05-03-2018).C

--- swap 2 number(05-03-2018).C
+++ swap 2 number(05-03-2018).C
@@ -5,7 +5,9 @@ int main()
     scanf("%d %d",&n1,&n2);
     n1=n1^n2;                                  
     n2=n1^n2;                                   
-    n1=n1^n2;                                    
+    n1=n1^n2;  
+    
     printf("%d %d",n1,n2);
+    
     return 0;
 }
