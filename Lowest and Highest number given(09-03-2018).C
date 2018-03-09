f17e659210c08126a42906edfc6d256a9261ac8a
vicky <35688357+vigneshlove@users.noreply.github.com>
2018-03-09 13:39:22 +0530

    Create Lowest and Highest number given(09-03-2018).C

---
+++ Lowest and Highest number given(09-03-2018).C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(void) 
+{
+	int array[100];
+	int i,n;
+	printf("Enter the size of the array:");
+	scanf("%d",&n);
+	printf("\n The array elements are:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	int max=array[0];
+	int min=array[0];
+	for(i=1;i<n;i++)
+	{
+		if(array[i]>max)
+		{
+			max=array[i];
+		}
+		if(array[i]<min)
+		{
+			min=array[i];
+		}
+	}
+	printf("\n%d",min);
+	printf("\n%d",max);
+
+	return 0;
+}
