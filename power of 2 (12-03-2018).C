a2603f51e7a0043e048f0140aee80c791e009c9f
vicky <35688357+vigneshlove@users.noreply.github.com>
2018-03-12 15:51:41 +0530

    Create power of 2 (12-03-2018).C

---
+++ power of 2 (12-03-2018).C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(void) 
+{
+	int num,flag=0,temp;
+	printf("Enter the number");
+	scanf("%d",&num);
+	temp=num;
+	while(temp!=1)
+	{
+		if(temp%2!=0)
+		{
+			flag=1;
+			break;
+			
+		}
+		temp=temp/2;
+	}
+	if(flag==0)
+	{
+		printf("\n %d is a power of 2 so",num);
+		printf("\n YES");
+	}
+	else if(flag==1)
+	{
+		printf("\n %d is not a power of 2 so",num);
+			printf("\n NO");
+	}
+	return 0;
+}
