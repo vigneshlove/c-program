4c11a1e093858191ac3d9f840b4434f3d770eb6a
vicky <35688357+vigneshlove@users.noreply.github.com>
2018-03-12 15:44:31 +0530

    Create Sum(12-03-2018).C

---
+++ Sum(12-03-2018).C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) 
+{
+	int n1,n2,sum;
+	printf("\n Enter the first number:");
+	scanf("%d",&n1);
+	printf("\n Enter the second number:");
+	scanf("%d",&n2);
+	sum=n1+n2;
+	printf("%d",sum);
+	return 0;
+}
