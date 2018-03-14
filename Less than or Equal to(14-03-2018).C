e63735d26a855a4501b03b718edc68653cbfc51c
vicky <35688357+vigneshlove@users.noreply.github.com>
2018-03-14 03:33:06 -0700

    Create Less than or Equal to(14-03-2018).C

---
+++ Less than or Equal to(14-03-2018).C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+	int num,even;
+	printf("Enter the number:");
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+		even=num-2;	
+	}
+	else
+	{
+		even=num-1;
+	}
+	printf("\n%d",even);
+	return 0;
+}
