aed563e556c38759e1a0434e75c06dedd49ea003
vicky <35688357+vigneshlove@users.noreply.github.com>
2018-03-14 03:26:41 -0700

    Create Digit(14-03-2018).C

---
+++ Digit(14-03-2018).C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+	int num,temp,sum=0;
+	printf("Enter the number");
+	scanf("%d",&num);
+	temp=num;
+	while(num!=0)
+	{
+		temp=num%10;
+		sum=sum+temp;
+		num=num/10;
+ 
+	}
+	printf("\n%d",sum);
+	return 0;
+}
