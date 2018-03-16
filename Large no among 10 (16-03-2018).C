57a0220768e60365589a414e825b5e009ab7ae44
vicky <35688357+vigneshlove@users.noreply.github.com>
2018-03-16 03:37:23 -0700

    Create Large no among 10 (16-03-2018).C

---
+++ Large no among 10 (16-03-2018).C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+	int i,temp1;
+	int a[10];
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+		
+		
+			if(a[i]>a[i+1])
+			{
+				temp1=a[i];
+				a[i]=a[i+1];
+				a[i+1]=temp1;
+			}
+			
+	
+	}
+	printf("%d",a[i]);
+	return 0;
+}
