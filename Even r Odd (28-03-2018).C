0f08efc05cfa5b6d0591526ddbf1eea24a9ba77a
vicky <35688357+vigneshlove@users.noreply.github.com>
2018-03-28 16:08:15 +0530

    Update Even r Odd (28-03-2018).C

--- Even r Odd (28-03-2018).C
+++ Even r Odd (28-03-2018).C
@@ -3,18 +3,18 @@
 int main(void) 
 {
 	int n1,n2,diff;
-	printf("enter the number1:");
+	printf("Enter the number1:");
 	scanf("%d",&n1);
-	printf("enter the number2:");
+	printf("Enter the number2:");
 	scanf("%d",&n2);
 	diff=n1-n2;
 	if(diff%2==0)
 	{
-		printf("\neven");
+		printf("\nEven");
 	}
 	else
 	{
-		printf("\nodd");
+		printf("\nOdd");
 	}
 	return 0;
 }
