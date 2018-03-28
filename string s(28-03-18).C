304b694e94db34a9d708579d50c167ae104afa77
vicky <35688357+vigneshlove@users.noreply.github.com>
2018-03-28 15:45:52 +0530

    Update string s(28-03-18).C

--- string s(28-03-18).C
+++ string s(28-03-18).C
@@ -3,7 +3,7 @@ int main(void)
 {
 	char str[20];
 	int k,i;
-	printf("enter the string and the k value:");
+	printf("Enter the string and the k value:");
 	scanf("%s %d",str,&k);
 	for(i=0;i<k;i++)
 	{
